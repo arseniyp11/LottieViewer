09dcef3b891688419d0746ea923d8246c91471a2
Arseniy <arseniyparakhnych11@gmail.com>
2026-08-27 16:15:56 +0300

    Initial Commit

---
+++ LottieViewer/LottieViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieViewerApp.swift
+//  LottieViewer
+//
+//  Created by Arseniy on 27.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct LottieViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
